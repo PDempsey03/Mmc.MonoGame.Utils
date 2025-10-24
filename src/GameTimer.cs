4e27cc18ccdb17733e484b3bdab3da175adc362d
PDempsey03 <dempsey.202@osu.edu>
2025-10-24 14:36:51 -0400

    Added initial project with 1 utility class

---
+++ src/GameTimer.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Mmc.MonoGame.Utils
+{
+    public class GameTimer : IUpdateable
+    {
+        protected int _updateOrder = 0;
+        protected bool _enabled = true;
+
+        protected readonly List<ScheduledAction> scheduledActions = [];
+        protected int scheduleActionsCurrentIndex = 0;
+
+        public event EventHandler<EventArgs>? EnabledChanged;
+        public event EventHandler<EventArgs>? UpdateOrderChanged;
+
+        public event EventHandler<GameTimerUpdateEventArgs>? Updated;
+        public event EventHandler<EventArgs>? TimerCompleted;
+
+        public TimeSpan CurrentTime { get; set; } = TimeSpan.Zero;
+
+        public TimeSpan Duration { get; set; } = TimeSpan.Zero;
+
+        public bool Repeating { get; set; } = false;
+
+        public TimeSpan TimeRemaining => Duration - CurrentTime;
+
+        public bool Enabled
+        {
+            get => _enabled;
+            set
+            {
+                if (_enabled != value)
+                {
+                    _enabled = value;
+                    EnabledChanged?.Invoke(this, new EventArgs());
+                }
+            }
+        }
+
+        public int UpdateOrder
+        {
+            get => _updateOrder;
+            set
+            {
+                if (value != _updateOrder)
+                {
+                    _updateOrder = value;
+                    UpdateOrderChanged?.Invoke(this, new EventArgs());
+                }
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (!Enabled) return;
+
+            // update to new time
+            CurrentTime.Add(gameTime.ElapsedGameTime);
+
+            // invoke event called on every update cycle
+            Updated?.Invoke(this, new GameTimerUpdateEventArgs(CurrentTime, Duration));
+
+            // check for actions called on specific times
+            CheckScheduledActions();
+
+            // check for timer completion
+            if (CurrentTime >= Duration)
+            {
+                HandleTimerCompletion();
+            }
+        }
+
+        protected virtual void CheckScheduledActions()
+        {
+            while (true)
+            {
+                var currentScheduledAction = GetCurrentScheduledAction();
+
+                if (!currentScheduledAction.HasValue || CurrentTime < currentScheduledAction.Value.TriggerTime)
+                {
+                    break;
+                }
+
+                currentScheduledAction.Value.Action?.Invoke();
+                scheduleActionsCurrentIndex++;
+            }
+        }
+
+        public void SetDurationInMilliSeconds(double milliSeconds) => Duration = TimeSpan.FromMilliseconds(milliSeconds);
+
+        public void SetDurationInSeconds(double seconds) => SetDurationInMilliSeconds(seconds * 1000);
+
+        public void SetDurationInMinutes(double minutes) => SetDurationInSeconds(minutes * 60);
+
+        public void SetDurationInMinutesAndSeconds(double minutes, double seconds) => SetDurationInSeconds(minutes * 60 + seconds);
+
+        private void HandleTimerCompletion()
+        {
+            Reset();
+            Enabled = Repeating;
+            TimerCompleted?.Invoke(this, new EventArgs());
+        }
+
+        public void Start()
+        {
+            Reset();
+            Enabled = true;
+        }
+
+        public void Pause()
+        {
+            Enabled = false;
+        }
+
+        public void Resume()
+        {
+            Enabled = true;
+        }
+
+        public void Reset()
+        {
+            CurrentTime = TimeSpan.Zero;
+            Enabled = false;
+            scheduleActionsCurrentIndex = 0;
+        }
+
+        // allow custom interval actions
+        public void ScheduleTimeAction(TimeSpan time, Action action)
+        {
+            scheduledActions.Add(new ScheduledAction(time, action));
+
+            // resort the list for proper optimization
+            scheduledActions.Sort((a, b) => a.TriggerTime.CompareTo(b.TriggerTime));
+        }
+
+        public void ClearScheduledTimeActions()
+        {
+            scheduledActions.Clear();
+        }
+
+        private ScheduledAction? GetCurrentScheduledAction()
+        {
+            return (scheduleActionsCurrentIndex >= 0 && scheduleActionsCurrentIndex < scheduledActions.Count) ? scheduledActions[scheduleActionsCurrentIndex] : null;
+        }
+
+        protected readonly struct ScheduledAction
+        {
+            public TimeSpan TriggerTime { get; init; }
+
+            public Action Action { get; init; }
+
+            public ScheduledAction(TimeSpan triggerTime, Action action)
+            {
+                TriggerTime = triggerTime;
+                Action = action;
+            }
+        }
+    }
+}
---
+++ src/GameTimerUpdateEventArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace Mmc.MonoGame.Utils
+{
+    public class GameTimerUpdateEventArgs : EventArgs
+    {
+        public TimeSpan CurrentTime { get; init; }
+
+        public TimeSpan Duration { get; init; }
+
+        public GameTimerUpdateEventArgs(TimeSpan currentTime, TimeSpan duration)
+        {
+            CurrentTime = currentTime;
+            Duration = duration;
+        }
+    }
+}
