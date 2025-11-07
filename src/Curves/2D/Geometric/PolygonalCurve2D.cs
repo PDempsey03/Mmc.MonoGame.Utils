9a2e041ed442d142f837f3d483582a324374b939
PDempsey03 <dempsey.202@osu.edu>
2025-11-07 12:55:47 -0500

    Add polygonal curve class definitions

---
+++ src/Curves/2D/Geometric/PolygonalCurve2D.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Mmc.MonoGame.Utils.Curves._2D.Geometric
+{
+    public class PolygonalCurve2D : Curve2D
+    {
+        public override Vector2 GetPoint(float t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Vector2 GetTangent(float t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
